6eb6311e9694be6302bda28d22e77be960b6b9ab
lucasrocha729 <lucasrocha.0822@gmail.com>
2022-04-08 23:10:43 -0300

    chore: added linebreak style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir : __dirname, 
@@ -17,6 +18,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "linebreak-style": 0,
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
