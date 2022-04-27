6e15cf0b5680584626287409f8a88e0b72f78abc
lucasrocha729 <lucasrocha.0822@gmail.com>
2022-04-26 22:25:12 -0300

    fix: disabled SSL certificate

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  //Disable SSL
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
   await app.listen(3000);
 }
 bootstrap();
