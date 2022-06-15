9f218dd716b2bd61258c1eda6756869c75af47d2
lucasrocha729 <lucasrocha.0822@gmail.com>
2022-06-14 21:16:57 -0300

    refactor(rabbitmq): added rabbitmq in send characterInfos

--- src/raider-io/raider-io.module.ts
+++ src/raider-io/raider-io.module.ts
@@ -1,10 +1,26 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { RaiderIoController } from './raider-io.controller';
 import { RaiderIoService } from './raider-io.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    ClientsModule.register([
+      {
+        name: 'CLIENT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'characterInfosQueue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   providers: [RaiderIoService],
   controllers: [RaiderIoController],
 })
--- src/raider-io/raider-io.service.ts
+++ src/raider-io/raider-io.service.ts
@@ -4,6 +4,7 @@ import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
 import { buildObject } from 'objectypes';
 import { Character } from './models/character.model';
 import { lastValueFrom } from 'rxjs';
+import { ClientProxy, RmqRecordBuilder } from '@nestjs/microservices';
 
 @Injectable()
 export class RaiderIoService {
@@ -12,6 +13,7 @@ export class RaiderIoService {
   constructor(
     @Inject(forwardRef(() => HttpService))
     private readonly httpService: HttpService,
+    @Inject('CLIENT_SERVICE') private client: ClientProxy,
   ) {}
 
   async sendInfos(payload: SendInfosDTO): Promise<Character> {
@@ -34,12 +36,18 @@ export class RaiderIoService {
       const characterInfos = buildObject(Character, promiseResponse.data);
       characterInfos.name = payload.name;
 
-      await lastValueFrom(
-        this.httpService.post(
-          process.env.MANAGEMENT_GROUPS_URL,
-          characterInfos,
-        ),
-      );
+      const record = new RmqRecordBuilder(characterInfos)
+        .setOptions({
+          headers: {
+            ['x-version']: '1.0.0',
+          },
+          priority: 1,
+        })
+        .build();
+
+      this.client.emit('characterInfosQueue', record);
+
+      this.logger.log('Message publish in characterInfosQueue');
 
       return characterInfos;
     } catch (error) {
